4b3646eb07a8c2f08587f0156057472b807d59e1
Kadlag <punam.kadlag@capgemini.com>
2019-02-07 18:56:42 +0530

    add files

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SigninComponent } from './signin/signin.component';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Annapoorna-Project';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { SigninModule } from './signin/signin.module';
+import { CartModule } from './cart/cart.module';
+@NgModule({
+  declarations: [
+    AppComponent
+    
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SigninModule,
+    CartModule
+    // RouterModule.forRoot([
+    //   {path:"signin", component:SigninComponent},
+    //   { path: '', redirectTo: 'signin', pathMatch: 'full' },
+    //   { path: '**', redirectTo: 'signin', pathMatch: 'full' }
+      
+    // ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
